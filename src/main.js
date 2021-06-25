737649694cbedb28ba75e88ae8657786ec171a18
H0L1C <yan.hlc0112@gmail.com>
2021-06-25 20:41:05 +0900

    add smootscroll polyfill

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import VueMq from "vue3-mq";
 import VueLazyLoad from 'vue3-lazyload'
+import smoothscroll from 'smoothscroll-polyfill';
+
+smoothscroll.polyfill();
 
 const app = createApp(App)
 
